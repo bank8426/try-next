2cccbbf1941b32b2158d067d91ca194a7e61b784
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-05-23 13:29:56 +0700

    add blog detail page

---
+++ app/blog/[id]/page.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+async function getBlogById(id) {
+  const response = await fetch(
+    `${process.env.MOCKAPI_BASE_URL}/${id}`
+    // { method: "GET" }
+  );
+  console.log("asdasdasdas");
+
+  if (!response.ok) {
+    throw new Error(`Cannot fetch blog data with id : ${id}`);
+  }
+
+  return response.json();
+}
+
+export default async function ({ params }) {
+  let blog = await getBlogById(params.id);
+
+  console.log(blog);
+
+  return (
+    <div>
+      <div>
+        {blog.id}. {blog.name}
+      </div>
+      <div>Content : {blog.content}</div>
+
+      <div>Author : {blog.author}</div>
+      <div>Description : {blog.description}</div>
+      <Image src={blog.imageUrl} height={100} width={100} alt="" />
+    </div>
+  );
+}
